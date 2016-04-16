3517633bbd80b77398cf4c48310e024504147e8e
OnlyHughMan <hughvidler@hotmail.com>
2016-04-16 19:47:16 +0800

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  arraylength = array.length
+  i = 0
+  j = 0
+  select = []
+  while i < arraylength
+    if yield(array[i]) == true
+      select[j] = (array[i])
+      j+=1
+    end
+    i+=1
+  end
+  return select
 end
