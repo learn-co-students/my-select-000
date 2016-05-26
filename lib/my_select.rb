a5b5ef9f0dcf79fcc578262f74fe841f7f92b871
Robert Horrigan <roberthorrigan@gmail.com>
2016-05-25 21:28:24 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select collection
+  i = 0
+  new_collection = []
+  while i < collection.length do
+    if collection[i] % 2 == 0
+      new_collection << collection[i]
+    end
+    i += 1
+  end
+  new_collection
 end
